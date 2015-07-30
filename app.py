60a76a383b7feaaf93928577be42d746656b165b
Roman Kos <win.maklin@gmail.com>
2015-07-30 20:40:12 +0300

    Test

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 @app.route('/hello/<name>')
-def hello(name=None):
+def hello_amigos(name=None):
     return render_template("index.html", name=name)
 
 if __name__ == "__main__":
